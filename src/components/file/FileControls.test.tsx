14c58b6517ae3e5e23161140d71390bed3f65c8b
Anders Nylund <anders@nylund.co>
2023-03-02 16:27:41 +0200

    Improve FileControls tests (#32)

--- src/components/file/FileControls.test.tsx
+++ src/components/file/FileControls.test.tsx
@@ -1,14 +1,31 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
+import { browserEnv } from '~/config/browser-env';
 import { TestProvider } from '~/test/utils';
 import { FileControls } from './FileControls';
 
 describe('<FileControls />', () => {
-  it('renders', async () => {
+  beforeEach(() => {
+    Object.defineProperty(browserEnv, 'NEXT_PUBLIC_SITE_ENV', { value: 'test' });
+  });
+
+  it('renders adding test route if site env is test', async () => {
     render(
       <TestProvider>
         <FileControls />
       </TestProvider>
     );
+    screen.getByText('Test');
+  });
+
+  it('does not render test route if site env is production', async () => {
+    Object.defineProperty(browserEnv, 'NEXT_PUBLIC_SITE_ENV', { value: 'production' });
+
+    render(
+      <TestProvider>
+        <FileControls />
+      </TestProvider>
+    );
+    expect(screen.queryByText('Test')).not.toBeInTheDocument();
   });
 });
