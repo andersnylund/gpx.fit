0a7e84cf1070d1153a20496e4174ff6fb55a1272
Anders Nylund <anders@nylund.co>
2023-03-03 13:20:36 +0200

    Change smoothened route on slider change (#33)

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -3,18 +3,21 @@ import { produce } from 'immer';
 import throttle from 'lodash.throttle';
 import { useEffect, useState } from 'react';
 import { z } from 'zod';
-import { Coordinate, setSelectedRoute } from '~/features/route';
-import { useAppDispatch } from '~/hooks';
+import { Coordinate, setSelectedRoute, setSmoothenedRoute } from '~/features/route';
+import { useAppDispatch, useAppSelector } from '~/hooks';
+import { smoothen } from '~/smoothen';
 import { AppDispatch } from '~/store';
 
 const sliderSchema = z.tuple([z.number(), z.number()]);
 type Values = z.infer<typeof sliderSchema>;
 
-const throttledOnChange = throttle((values: Values, track: Coordinate[], dispatch: AppDispatch) => {
+const throttledOnChange = throttle((values: Values, track: Coordinate[], treshold: number, dispatch: AppDispatch) => {
   const selectedRoute = produce(track, (draftTrack) => {
     return draftTrack.slice(values[0], values[1]);
   });
   dispatch(setSelectedRoute(selectedRoute));
+  const smoothened = smoothen(selectedRoute, treshold);
+  dispatch(setSmoothenedRoute(smoothened));
 }, 100);
 
 interface Props {
@@ -23,6 +26,7 @@ interface Props {
 
 export const RangeSlider = ({ route }: Props) => {
   const dispatch = useAppDispatch();
+  const treshold = useAppSelector((state) => state.treshold.treshold);
 
   const [values, setValues] = useState([0, 0]);
 
@@ -41,7 +45,7 @@ export const RangeSlider = ({ route }: Props) => {
         if (parsedValues.success) {
           const data = parsedValues.data;
           setValues(data);
-          throttledOnChange(data, route, dispatch);
+          throttledOnChange(data, route, treshold, dispatch);
         }
       }}
       variant="soft"
