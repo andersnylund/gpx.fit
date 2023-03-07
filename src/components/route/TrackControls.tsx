89fe0bd4cf4f3131bc995f7b1dc9d7ab49c8ce89
Anders Nylund <anders@nylund.co>
2023-03-07 12:03:25 +0200

    Add export button (#39)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/components/route/TrackControls.tsx
+++ src/components/route/TrackControls.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { useAppSelector } from '~/hooks';
 import { RangeSlider } from '../RangeSlider';
+import { ExportButton } from './ExportButton';
 import { Treshold } from './Treshold';
 
 export const TrackControls = () => {
@@ -12,6 +13,7 @@ export const TrackControls = () => {
 
   return (
     <Container>
+      <ExportButton />
       <Treshold />
       <RangeSlider route={route} />
     </Container>
@@ -19,10 +21,11 @@ export const TrackControls = () => {
 };
 
 const Container = styled.div`
+  align-items: flex-end;
   bottom: 0;
   display: flex;
-  align-items: flex-end;
   flex-direction: column;
+  gap: 16px;
   margin: 16px 32px;
   position: absolute;
   width: calc(100vw - 64px);
