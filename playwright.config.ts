111a01f3d20ca852c8018081a4b9875d7df476d8
Anders Nylund <anders@nylund.co>
2023-06-02 10:33:11 +0300

    Simplify playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,6 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000';
-// eslint-disable-next-line no-console
-console.log(`ℹ️ Using base URL "${baseUrl}"`);
-
-const opts = {
-  // launch headless on CI, in browser locally
-  headless: !!process.env.CI || !!process.env.PLAYWRIGHT_HEADLESS,
-  // collectCoverage: !!process.env.PLAYWRIGHT_HEADLESS
-};
 
 const config: PlaywrightTestConfig = {
   testDir: './playwright',
@@ -20,8 +12,6 @@ const config: PlaywrightTestConfig = {
   use: {
     ...devices['Desktop Chrome'],
     baseURL: baseUrl,
-    headless: opts.headless,
-    video: 'on',
   },
 };
 
